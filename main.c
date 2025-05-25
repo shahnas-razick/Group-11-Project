05ac7dedf9153dfe975618ecd261cd4416a3317f
Dulaksha99 <dulakshar2@gmail.com>
2025-05-25 11:59:17 +0530

    add term colors

--- main.c
+++ main.c
@@ -30,23 +30,25 @@ int authenticateUser(int userType, char* userId, char* password) {
     fclose(file);
     return 0;
 }
-
+// banner and menu
 void displayMenu() {
-    
-    printf(" _        _            _              _          \n");
+
+    printf("\x1b[34m _        _            _              _          \n");
     printf("| |      |_   _| |  |   | ____|    / ____|      /\\     |   \\    |  ____|  \n");
     printf("| |        | |   | |    | |      | |         /  \\    | |) | | |     \n");
     printf("| |        | |   |  |   |  |     | |        / /\\ \\   |  _  /  |  |    \n");
     printf("| |____   _| |_  | |      | |____    | |____   /  \\  | | \\ \\  | |__   \n");
-    printf("|______| |_____| |_|      |______|    \\_____| /_/    \\_\\ |_|  \\_\\ |______|\n");
+    printf("|______| |_____| |_|      |______|    \\_____| /_/    \\_\\ |_|  \\_\\ |______|\x1b[0m\n");
 
-    printf("\n=== Medical Record System Login ===\n");
-    printf("1. Admin\n");
+
+    printf("\n\x1b[31m=== Medical Record System Login ===\n");
+    printf("\x1b[33m1. Admin\n");
     printf("2. Doctor\n");
     printf("3. Lab Assistant\n");
     printf("4. Exit\n");
     printf("Enter your choice (1-4): ");
 }
+
 int main(){
 
 }
