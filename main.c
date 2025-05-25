9d0498e99402b56818f7a6233dd5549e68496998
Dulaksha99 <dulakshar2@gmail.com>
2025-05-25 11:34:43 +0530

    login function implemented

---
+++ main.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define MAX_LENGTH 50
+
+// login function
+int authenticateUser(int userType, char* userId, char* password) {
+    FILE* file = fopen("./data/users.txt", "r");
+    if (file == NULL) {
+        printf("Error: Cannot open users file\n");
+        return 0;
+    }
+
+    char line[256];
+    int storedType;
+    char storedId[MAX_LENGTH], storedPass[MAX_LENGTH];
+
+    while (fgets(line, sizeof(line), file)) {
+        sscanf(line, "%d,%[^,],%[^\n]", &storedType, storedId, storedPass);
+        
+        if (storedType == userType && 
+            strcmp(storedId, userId) == 0 && 
+            strcmp(storedPass, password) == 0) {
+            fclose(file);
+            return 1;
+        }
+    }
+    fclose(file);
+    return 0;
+}
+
+int main(){
+
+}
